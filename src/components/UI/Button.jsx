596af5d18e703b487287473c6642306990701821
Houssam Ouhra <houssamblur04@gmail.com>
2025-10-10 22:01:49 +0100

    refactor(ui): make Button component reusable and update Header layout

--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -1,6 +1,15 @@
-const Button = ({ title, symbole }) => {
+const Button = ({ title, symbole, variant = 'primary', className = '', ...props }) => {
+  const baseStyle =
+    'font-Gontserrat font-semibold text-md md:text-basemy-5 px-4 py-2 cursor-pointer rounded-md ';
+
+  const variants = {
+    primary: 'bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100 transition',
+    cancel:
+      'border border-stone-400 bg-stone-400 text-stone-700 hover:bg-stone-300 hover:text-stone-400 transition',
+  };
+
   return (
-    <button className=' flex items-center gap-3 bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100 my-5 px-4 py-2 transition  cursor-pointer rounded-md  font-Gontserrat font-semibold text-md md:text-base '>
+    <button {...props} className={`${baseStyle} ${variants[variant]} ${className}`}>
       {symbole}
       <span>{title}</span>
     </button>
--- src/components/UI/Header.jsx
+++ src/components/UI/Header.jsx
@@ -1,24 +1,20 @@
 import ClipboardImg from '@/assets/imgs/no-projects.png';
 import Button from '@/components/UI/Button';
-import ProjectsSidebar from '@/components/layout/ProjectsSidebar';
 
-const Header = () => {
+const Header = ({ onOpen }) => {
   return (
-    <section className='flex h-screen '>
-      <ProjectsSidebar />
-      <div className='flex-1 flex justify-center items-center'>
-        <header className='flex-1 flex flex-col justify-center items-center gap-4'>
-          <img className='w-22 h-22' src={ClipboardImg} alt='empty-clipboard' />
-          <h2 className='text-2xl font-semibold font-Gontserrat text-stone-600'>
-            No Project Selected
-          </h2>
-          <p className='text-lg font-normal font-Gontserrat text-stone-400'>
-            Select a project or get started with a new one
-          </p>
-          <Button title={'Create new project'} />
-        </header>
+    <header className='flex-1 flex justify-center items-center'>
+      <div className='flex-1 flex flex-col justify-center items-center gap-4'>
+        <img className='w-22 h-22' src={ClipboardImg} alt='empty-clipboard' />
+        <h2 className='text-2xl font-semibold font-Gontserrat text-stone-600'>
+          No Project Selected
+        </h2>
+        <p className='text-lg font-normal font-Gontserrat text-stone-400'>
+          Select a project or get started with a new one
+        </p>
+        <Button onClick={onOpen} title={'Create new project'} />
       </div>
-    </section>
+    </header>
   );
 };
 
